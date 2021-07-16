28466e01681c9b0d7c9d2c95625f7b554da2a002
Sorrapat <85327439+Sorrapat@users.noreply.github.com>
2021-07-16 10:00:22 +0700

    Update CS01-7.py

---
+++ CS01-7.py
@@ -0,0 +1,23 @@
+A=int(input("Enter Number:"))
+if(A <= 10):
+    print("บ่ผ่าน")
+elif(A <= 20):
+    print("ปรับปรุง")
+else :
+    print("ผ่าน")
+
+B=int(input("Enter Number:"))
+if(B <= 10):
+    print("บ่ผ่าน")
+elif(B <= 20):
+    print("ปรับปรุง")
+else :
+    print("ผ่าน")
+
+C=int(input("Enter Number:"))
+if(C <= 10):
+    print("บ่ผ่าน")
+elif(C <= 20):
+    print("ปรับปรุง")
+else :
+    print("ผ่าน")
