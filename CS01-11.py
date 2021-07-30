52834473d0f1dc46fd4511e1f84acc95d7620b70
Sorrapat <85327439+Sorrapat@users.noreply.github.com>
2021-07-30 09:28:42 +0700

    Update CS01-11.py

--- CS01-11.py
+++ CS01-11.py
@@ -1,6 +1,4 @@
-for J in range(2,10,2):
-    print("This is J",J)
-    for L in range(10):
-        print("This is J",L)
-    
-Cars = ["Honda","Porshe","Lamborgini"]
+for x in range(0,5):
+    for y in range(0,x+1):
+        print("*",end="")
+    print("\n")
