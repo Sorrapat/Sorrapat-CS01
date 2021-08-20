6f87994946c1288ef94c106af7bf843c18ec8c50
Sorrapat <85327439+Sorrapat@users.noreply.github.com>
2021-08-20 09:30:31 +0700

    Update CS01-15.py

--- CS01-15.py
+++ CS01-15.py
@@ -4,3 +4,10 @@ def my_function(food):
 
 fruits = ["apple","banana","orange"]
 my_function(fruits)
+
+Superlist = [5,10,15,20,25,50,20]
+for i in range(len(Superlist)):
+    print(i)
+    if (Superlist[i] == 20):
+        Superlist[i] = 200
+print(Superlist)
